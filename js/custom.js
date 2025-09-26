ac203664771710a65d72e31c3278d0170e79fe0b
Papernoia <2023238176@qq.com>
2025-09-27 02:18:12 +0800

    Site updated: 2025-09-27 02:18:11

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,21 @@
-document.getElementById('card-info-btn').onclick = function() {
-  var popup = document.querySelector('.card-info-popup-img');
-  if (popup.style.display === 'none' || popup.style.display === '') {
-    popup.style.display = 'block';
-  } else {
-    popup.style.display = 'none';
-  }
-};
+document.addEventListener('DOMContentLoaded', function() {
+  // 为 cardauthor.pug 的按钮绑定事件
+  document.getElementById('card-info-btn-author').onclick = function() {
+    var popup = document.querySelector('.card-widget .card-info-popup-img');
+    if (popup.style.display === 'none' || popup.style.display === '') {
+      popup.style.display = 'block';
+    } else {
+      popup.style.display = 'none';
+    }
+  };
+
+  // 为 sidebar.pug 的头像绑定事件
+  document.getElementById('card-info-btn-sidebar').onclick = function() {
+    var popup = document.querySelector('#sidebar .card-info-popup-img');
+    if (popup.style.display === 'none' || popup.style.display === '') {
+      popup.style.display = 'block';
+    } else {
+      popup.style.display = 'none';
+    }
+  };
+});
